eee332a57aa04b3b40b3c64a4293fba8273ffcb0
nakool <alain.krzeczowski@gmail.com>
2025-06-25 15:48:58 +0200

    Add some scripts

---
+++ scripts/ping_ansible_hosts.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Activate python virtualenv
+if [[ -d .venv ]]; then
+  source .venv/bin/activate
+fi
+
+ANSIBLE_INVENTORY=ansible/inventories/inventory.ini
+
+# Launch ansible playbook
+ansible-playbook \
+    -i ${ANSIBLE_INVENTORY} \
+    ./ansible/ping-playbook.yml
