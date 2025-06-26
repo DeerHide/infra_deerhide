42c101b43a487f8c10124a4c7534fc2f0ee442bb
nakool <alain.krzeczowski@gmail.com>
2025-06-26 17:42:51 +0200

    feat: 🎸 Add script to encrypt vars in ansible-vars file

---
+++ scripts/encrypt_ansible-vault_file.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Activate python virtualenv
+if [[ -d .venv ]]; then
+  source .venv/bin/activate
+fi
+
+# Colors
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+
+# Load environment variables from .env file if it exists
+if [[ -f .env ]]; then
+  export $(cat .env | xargs)
+fi
+
+# Check if the ANSIBLE_VAULT_PASSWORD_FILE environment variable is set
+if [[ -z "${ANSIBLE_VAULT_PASSWORD_FILE}" ]]
+then
+  echo -e "$RED ANSIBLE_VAULT_PASSWORD_FILE is not set $NC"
+  exit 1
+fi
+
+variable_name_to_encrypt=${1:-""}
+
+if [[ -z "${variable_name_to_encrypt}" ]]
+then
+  echo -e "$RED Please provide the variable name to encrypt $NC"
+  exit 1
+fi
+
+function encrypt_value(){
+  local variable_name_to_encrypt=$1
+  actual_variable_value=$(yq e ".${variable_name_to_encrypt} | sub(\"\n$\",\"\")" ansible-vars.yml )
+  echo -n "${actual_variable_value}" | ansible-vault \
+    encrypt_string \
+    --encrypt-vault-id=default \
+    --vault-password-file ${ANSIBLE_VAULT_PASSWORD_FILE} \
+    -n "${variable_name_to_encrypt}"
+}
+
+new_value=$(encrypt_value ${variable_name_to_encrypt})
+yq e -i ".${variable_name_to_encrypt} = \"${new_value}\"" ansible-vars.yml
+sed -i 's/ |-//g' ansible-vars.yml
+yq -i e "." ansible-vars.yml
