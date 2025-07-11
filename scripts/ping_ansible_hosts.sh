2ed475c29767b9053636539ea61efb0b07e7a9c8
nakool <alain.krzeczowski@gmail.com>
2025-07-11 19:28:09 +0200

    fix: 🐛 add ansible vault and ansible-vars in scripts

--- scripts/ping_ansible_hosts.sh
+++ scripts/ping_ansible_hosts.sh
@@ -7,7 +7,21 @@ fi
 
 ANSIBLE_INVENTORY=ansible/inventories/inventory.ini
 
+# Load environment variables from .env file if it exists
+if [[ -f .env ]]; then
+  export $(cat .env | xargs)
+fi
+
+# Check if the ANSIBLE_VAULT_PASSWORD_FILE environment variable is set
+if [[ -z "${ANSIBLE_VAULT_PASSWORD_FILE}" ]]
+then
+  echo "$RED ANSIBLE_VAULT_PASSWORD_FILE is not set $NC"
+  exit 1
+fi
+
 # Launch ansible playbook
 ansible-playbook \
     -i ${ANSIBLE_INVENTORY} \
+    --vault-id="default@${ANSIBLE_VAULT_PASSWORD_FILE}" \
+    -e @ansible-vars.yml \
     ./ansible/ping-playbook.yml
